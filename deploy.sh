4c032957d674988b1a0d8bd8198cf8a3219ef1b3
Richard Mortier <mort@cantab.net>
2015-02-17 15:30:39 +0100

    deploy: update in line with decks and tutorial

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/sh -ex
 
+VM=mir-www
+
+XM=xm
 KERNEL=`pwd`/xen/`cat xen/latest`
-sed -e "s,@KERNEL@,$KERNEL/mir-www.xen,g" < xl.conf.in > $KERNEL/xl.conf
+sed -e "s,@VM@,$VM,g; s,@KERNEL@,$KERNEL/$VM.xen,g" \
+    < $XM.conf.in \
+    > $KERNEL/$XM.conf
+
 cd $KERNEL
-rm -f mir-www.xen
-bunzip2 -k mir-www.xen.bz2
-sudo xl destroy mirage-www || true
-sudo xl create xl.conf
+rm -f $VM.xen
+bunzip2 -k $VM.xen.bz2
+
+sudo $XM destroy $VM || true
+sudo $XM create $XM.conf
